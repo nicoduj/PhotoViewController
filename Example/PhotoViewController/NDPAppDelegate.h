946d1c7d51482eea19ae5c3a23050e94f90f54a7
Nicolas Dujardin <nicolas.dujardin@gmail.com>
2018-02-19 09:58:08 +0100

    Initial commit

---
+++ Example/PhotoViewController/NDPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NDPAppDelegate.h
+//  PhotoViewController
+//
+//  Created by Nicolas Dujardin on 02/19/2018.
+//  Copyright (c) 2018 Nicolas Dujardin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NDPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/PhotoViewController/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PhotoViewController
+//
+//  Created by Nicolas Dujardin on 02/19/2018.
+//  Copyright (c) 2018 Nicolas Dujardin. All rights reserved.
+//
+
+@import UIKit;
+#import "NDPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NDPAppDelegate class]));
+    }
+}
